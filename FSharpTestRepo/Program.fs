9fabf66ddaad255699d1d1ad2a3e0e5f4cd766d1
Bart <bwj.kempen@student.avans.nl>
2024-04-26 10:03:14 +0200

    Add project files.

---
+++ FSharpTestRepo/Program.fs
@@ -0,0 +1,15 @@
+﻿type Symbol =
+    | Cross
+    | Nought
+
+let stringOfSymbol symbol =
+    match symbol with
+    | Cross -> "X"
+    | Nought -> "O"
+
+printf "%s" (stringOfSymbol Symbol.Cross)
+
+
+type Field =
+    | Empty
+    | Taken of Symbol
